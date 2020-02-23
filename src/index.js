0755a339061ec6fd129719f62de1a1837230c0ac
Kirill Scherbakoff <elkirrs@gmail.com>
2020-02-23 17:22:50 +0300

    feat: matrix code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (!matrix || matrix.length == 0) {
+        return [];
+    }
+    let arrNumber = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 != 0) {
+            arrNumber.concat(matrix[i].reverse());
+        }
+        arrNumber = arrNumber.concat(matrix[i]);
+    }
+    return arrNumber;
+};
